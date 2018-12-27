59be43c230f706b356fedb4abee632ddcce9a7ef
Gonçalo Santos <goncalossantos98@gmail.com>
2018-12-27 16:09:55 +0000

    [ControlApp] Use in-tree GLFW include during build

--- ControlApp/build.sh
+++ ControlApp/build.sh
@@ -9,7 +9,7 @@ then
     c++ -O2 -Iexternal/ external/imgui/imgui.cpp -c -o build/imgui.o
     c++ -O2 -Iexternal/ external/imgui/imgui_draw.cpp -c -o build/imgui_draw.o
     c++ -O2 -Iexternal/ external/imgui/imgui_widgets.cpp -c -o build/imgui_widgets.o
-    c++ -O2 -Iexternal/ external/imgui/imgui_impl_glfw.cpp -c -o build/imgui_impl_glfw.o
+    c++ -O2 -Iexternal/ -Iexternal/glfw/include external/imgui/imgui_impl_glfw.cpp -c -o build/imgui_impl_glfw.o
     c++ -O2 -Iexternal/ external/imgui/imgui_impl_opengl3.cpp -c -o build/imgui_impl_opengl3.o
 fi
 c++ -g3 -lGL -lX11 -ldl -lpthread -Iexternal/ main.cpp build/*.o -o build/ControlApp
