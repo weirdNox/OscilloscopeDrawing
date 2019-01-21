947a2a59f85139f2e06eb2c023f39de43b9bf6cd
Gonçalo Santos <goncalossantos98@gmail.com>
2019-01-21 17:57:07 +0000

    Move external dependencies to main folder

--- ControlApp/build.sh
+++ ControlApp/build.sh
@@ -2,11 +2,11 @@
 if [ ! -d "build/" ]
 then
     mkdir build
-    cc -O2 external/rglfw.c -c -o build/rglfw.o
-    c++ -O2 -Iexternal/ external/imgui/imgui.cpp -c -o build/imgui.o
-    c++ -O2 -Iexternal/ external/imgui/imgui_draw.cpp -c -o build/imgui_draw.o
-    c++ -O2 -Iexternal/ external/imgui/imgui_widgets.cpp -c -o build/imgui_widgets.o
-    c++ -O2 -Iexternal/ -Iexternal/glfw/include external/imgui/imgui_impl_glfw.cpp -c -o build/imgui_impl_glfw.o
-    c++ -O2 -Iexternal/ external/imgui/imgui_impl_opengl3.cpp -c -o build/imgui_impl_opengl3.o
+    cc -O2 ../External/rglfw.c -c -o build/rglfw.o
+    c++ -O2 -I../External/ ../External/imgui/imgui.cpp -c -o build/imgui.o
+    c++ -O2 -I../External/ ../External/imgui/imgui_draw.cpp -c -o build/imgui_draw.o
+    c++ -O2 -I../External/ ../External/imgui/imgui_widgets.cpp -c -o build/imgui_widgets.o
+    c++ -O2 -I../External/ -I../External/glfw/include ../External/imgui/imgui_impl_glfw.cpp -c -o build/imgui_impl_glfw.o
+    c++ -O2 -I../External/ ../External/imgui/imgui_impl_opengl3.cpp -c -o build/imgui_impl_opengl3.o
 fi
-c++ -g3 -lGL -lX11 -ldl -lpthread -Iexternal/ -I../Shared main.cpp build/*.o -o build/ControlApp
+c++ -g3 -lGL -lX11 -ldl -lpthread -I../External/ -I../Shared main.cpp build/*.o -o build/ControlApp
